fae851b12fe2efd65b353c64b3a08fc9b1a54718
PSD <psdstockss@gmail.com>
2025-12-14 22:24:26 +0200

    chore: cleanup duplicate router registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -88,9 +88,6 @@ app.use(subscriberRouter);
 app.use(settingsRouter);
 app.use(reminderRouter);
 app.use(wooRouter);
-import { reportsRouter } from './routes/reports';
-// ...
-app.use(wooRouter);
 app.use(cronRouter);
 app.use(reportsRouter);
 
